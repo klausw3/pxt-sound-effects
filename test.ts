08dcd683e62d544e8aef1f39bee2eab2a6a986ba
klausw3 <72374521+klausw3@users.noreply.github.com>
2020-10-06 14:30:32 -0700

    add test file (untested)

--- test.ts
+++ test.ts
@@ -1,1 +1,20 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+let sfx = soundEffects.createSound(WaveType.Square50, 1000, 440, 440)
+sfx.play()
+sfx.freq1 = sfx.freq0 * 2
+sfx.wave = WaveType.Sawtooth
+sfx.playUntilDone()
+
+let waves: number[] = []
+waves = [
+soundEffects.waveNumber(WaveType.Triangle),
+soundEffects.waveNumber(WaveType.Sawtooth),
+soundEffects.waveNumber(WaveType.Sine),
+soundEffects.waveNumber(WaveType.WhiteNoise),
+soundEffects.waveNumber(WaveType.Square10),
+soundEffects.waveNumber(WaveType.Square30),
+soundEffects.waveNumber(WaveType.Square50),
+soundEffects.waveNumber(WaveType.Cycle16),
+soundEffects.waveNumber(WaveType.Cycle32),
+soundEffects.waveNumber(WaveType.Cycle64),
+soundEffects.waveNumber(WaveType.TunableNoise)
+]
