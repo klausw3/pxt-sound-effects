bdb0de1943122ce394a090863299931689631d50
klausw3 <72374521+klausw3@users.noreply.github.com>
2020-10-06 10:29:41 -0700

    Update API to use numbers for waves

--- soundeffects.ts
+++ soundeffects.ts
@@ -2,14 +2,15 @@ namespace music {
     //% shim=music::queuePlayInstructions
     export function playInstructions(timeDelta: number, buf: Buffer) { }
 }
-
-enum WaveType {
+const enum WaveType {
     //% block="triangle"
     Triangle = 1,
     //% block="sawtooth"
     Sawtooth = 2,
     //% block="sine"
     Sine = 3,
+    //% block="white noise"
+    WhiteNoise = 5,
     //% block="square 10%"
     Square10 = 11,
     //% block="square 20%"
@@ -26,8 +27,8 @@ enum WaveType {
     Cycle32 = 17,
     //% block="cycle 64"
     Cycle64 = 18,
-    //% block="noise"
-    Noise = 4,
+    //% block="tunable noise"
+    TunableNoise = 4,
 }
 
 //% blockNamespace=soundEffects
@@ -65,13 +66,12 @@ class SoundBuffer {
 
     //% blockSetVariable="soundeffect"
     //% blockCombine
-    get wave() { return this._buf.getNumber(NumberFormat.UInt16LE, 0); }
-    //% blockSetVariable="soundeffect"
-    //% blockCombine
     set wave(v: WaveType) { this._buf.setNumber(NumberFormat.UInt16LE, 0, v); }
-
     //% blockSetVariable="soundeffect"
     //% blockCombine
+    get wave(): WaveType { return this._buf.getNumber(NumberFormat.UInt16LE, 0); }
+    //% blockSetVariable="soundeffect"
+    //% blockCombine weight=60
     get duration() { return this._buf.getNumber(NumberFormat.UInt16LE, 4); }
     //% blockSetVariable="soundeffect"
     //% blockCombine
@@ -100,6 +100,12 @@ class SoundBuffer {
     //% blockSetVariable="soundeffect"
     //% blockCombine
     set vol1(v: number) { this._buf.setNumber(NumberFormat.UInt16LE, 8, v); }
+    //% blockSetVariable="soundeffect"
+    //% blockCombine weight=10
+    get waveNumber() { return this._buf.getNumber(NumberFormat.UInt16LE, 0); }
+    //% blockSetVariable="soundeffect"
+    //% blockCombine weight=10
+    set waveNumber(v: number) { this._buf.setNumber(NumberFormat.UInt16LE, 0, v); }
 }
 
 /**
@@ -109,19 +115,24 @@ class SoundBuffer {
 namespace soundEffects {
     /**
      * Creates a sound effect.
-     * @param wave wave form for the effect; eg: WaveType.Noise
+     * @param wave wave form for the effect; eg: WaveType.Square50
      * @param duration [0-99999] sound duration in milliseconds; eg: 1000
      * @param f0 [1-20000] starting frequency in Hz (cycles/second); eg: 440
      * @param f1 [1-20000] ending frequency in Hz; eg: 440
      * @param v0 [0-1023] starting volume; eg: 255
      * @param v1 [0-1023] ending volume; eg: 0
      */
-    //% blockId="create_sound" block="sound wave %wave for %duration ms, freq %f0 to %f1||, vol %v0 to %v1"
+    //% blockId="create_sound" block="sound wave %wave=waveNumber for %duration ms, freq %f0 to %f1||, vol %v0 to %v1"
     //% inlineInputMode=inline
     //% expandableArgumentMode="toggle"
     //% blockSetVariable=soundeffect
     //% weight=90
-    export function createSound(wave: WaveType, duration: number, f0: number, f1: number, v0: number=255, v1: number=0): SoundBuffer {
+    export function createSound(wave: number, duration: number, f0: number, f1: number, v0: number=255, v1: number=0): SoundBuffer {
         return new SoundBuffer(wave, duration, f0, f1, v0, v1);
+    }
+
+    //% blockId="waveNumber" block="%wave"
+    export function waveNumber(wave: WaveType): number {
+        return wave;
     }
 }
---
+++ tilemap.g.ts
@@ -0,0 +1,7 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+}
+// Auto-generated code. Do not edit.
